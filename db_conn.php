6cfe722c6285b38c1cce00d011a1980b57d16a7d
AgentBluewood <115904776+AgentBluewood@users.noreply.github.com>
2023-05-15 16:49:14 -0500

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,14 @@
+<?php
+//Connect to fuel database
+
+$sname= "localhost";
+$uname= "root";
+$pass = "";
+$db_name = "fuel_database";
+
+$conn = mysqli_connect($sname, $uname, $pass, $db_name);
+
+if (!$conn) {
+    die("Error: connection error. " . mysqli_connect_error());
+}
+
