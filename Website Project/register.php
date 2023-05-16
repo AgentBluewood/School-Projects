fa0adacfeab57262a0880c744d5e45f79de5dbb2
AgentBluewood <115904776+AgentBluewood@users.noreply.github.com>
2023-05-16 12:15:37 -0500

    Add files via upload
    Adding files to website project folder

---
+++ Website Project/register.php
@@ -0,0 +1,132 @@
+<?php
+session_start(); 
+require_once('db_conn.php');
+
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirm_password'])) {
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $username = validate($_POST['username']);
+    $password = validate($_POST['password']);
+    $confirm_password = validate($_POST["confirm_password"]);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT); //encrpypt user password 
+
+    //Validate all user entered information and ensure specifications are met
+    if (strlen($password) < 6) {
+        header("Location: register.php?error=Password must have at least 6 characters.");
+        exit();
+    } else if (empty($confirm_password)) {
+        header("Location: register.php?error=Please confirm your password.");
+        exit();
+    } else if (empty($password)) {
+        header("Location: register.php?error=Please enter your password.");
+        exit();
+    } else if (empty($username)) {
+        header("Location: register.php?error=Please enter your username.");
+        exit();
+    } else if ($password != $confirm_password) {
+        header("Location: register.php?error=Passwords did not match.");
+        exit();
+    } else if (strlen($password) > 200 || strlen($confirm_password) > 200) {
+        header("Location: register.php?error=Password is too long.");
+        exit();
+    } else if (strlen($username) > 200) {
+        header("Location: register.php?error=Username is too long.");
+        exit();
+    } else {
+        //Make sure username is not already taken before inserting account into database 
+        $sql = "SELECT id FROM UserCredentials WHERE username = ?";
+        if($stmt = mysqli_prepare($conn, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username); 
+            
+            $param_username = $username;
+
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    header("Location: register.php?error=This username is already taken.");
+                    exit();
+                } 
+            } else{
+                header("Location: register.php?error=Oops! Something went wrong. Please try again later.");
+                exit();
+            }
+            mysqli_stmt_close($stmt);
+        }
+
+        //If username is free, insert usernmae and encrypted password into database
+        $sql = "INSERT INTO UserCredentials (username, pass) VALUES (?, ?)";
+        if($stmt = mysqli_prepare($conn, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            $param_username = $username; 
+            $param_password = $password_hash; 
+            
+            if(mysqli_stmt_execute($stmt)){
+                $_SESSION['user_name'] = $username;
+                
+                $sql = "SELECT id FROM UserCredentials WHERE username = '$param_username' AND pass = '$param_password'";
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: profileManagement.php");
+                exit();
+            } else{
+                header("Location: register.php?error=Oops! Something went wrong. Please try again later.");
+                exit();
+            }
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    mysqli_close($conn);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Create Account</title>
+        <link rel="stylesheet" type="text/css" href="template.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Create Account</h1>
+                    <?php if (isset($_GET['error'])) { ?>
+                    <p class="error"><?php echo $_GET['error']; ?></p>
+                    <?php } ?>
+                    <hr />
+                    <p>Please fill out this form to create an account</p>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="username" class="form-control" placeholder="Username" required>
+                        </div>    
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" placeholder="Password" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input type="password" name="confirm_password" class="form-control" placeholder="Confirm Password" required>
+                        </div>
+                        <div class="form-group">
+                            <button name = "submit" class="registerbtn">Register Account</button>
+                        </div>
+                        <div class="login">
+                        <p>Already have an account? <a href="index.php">Login here</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>    
+    </body>
+</html>
